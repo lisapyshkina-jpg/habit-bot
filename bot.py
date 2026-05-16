65c2908e6660c393f14847c848107be170a4a3df
Елизавета Джингл <elizavetadzingl@MacBook-Air-Elizaveta.local>
2026-05-16 15:42:03 +0300

    fix webhook conflict

--- bot.py
+++ bot.py
@@ -155,6 +155,7 @@ async def click(callback: types.CallbackQuery):
 # --------------------
 
 async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
